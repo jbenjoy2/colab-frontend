a430bf90f9ef7a02cc32ea0063c0d1f6a96bf9b5
Jason Benjoya <jasonbenjoya@gmail.com>
2022-03-04 17:08:38 -0600

    mail additions

--- src/Components/Project/UserRequestCard.js
+++ src/Components/Project/UserRequestCard.js
@@ -3,27 +3,57 @@ import ColabAPI from "../../api/colabApi";
 import { useSelector } from "react-redux";
 import "./UserRequestCard.css";
 function UserRequestCard({ user, projectId, owner }) {
-  const { currentUser } = useSelector(st => st.user);
+  /**
+   *
+   * card component to render results from user search to request user collaborations
+   * props: user object, projectId, project owner
+   */
+  const { currentUser } = useSelector((st) => st.user);
   const [requests, setRequests] = useState([]);
   const [status, setStatus] = useState(null);
   const [isOwner, setIsOwner] = useState(false);
+  const [projectName, setProjectName] = useState(null);
+
+  // get all requests associated with project on page load, and whenever the status variable changs (on a request button click)
   useEffect(() => {
-    const getProjectRequests = async projectId => {
+    const getProjectRequests = async (projectId) => {
       const requests = await ColabAPI.getProjectRequests(projectId);
 
       if (requests.projRequests) {
         if (user.username === owner) {
           setIsOwner(true);
         }
-        setRequests(requests.projRequests.filter(req => req.recipient === user.username));
+        setRequests(
+          requests.projRequests.filter((req) => req.recipient === user.username)
+        );
       }
     };
     getProjectRequests(projectId);
   }, [status, owner, user.username, projectId]);
 
+  useEffect(() => {
+    const getProjectName = async (projectId) => {
+      const req = await ColabAPI.getProjectDetails(projectId);
+      console.log("req", req);
+      if (req) {
+        setProjectName(req.title);
+      }
+    };
+    getProjectName(projectId);
+  }, [projectId]);
+
+  // make a request on click and then rerender the request card
   const handleClick = async () => {
     const data = { project_id: +projectId, recipient: user.username };
+    console.log("title", projectName);
     await ColabAPI.makeRequest(currentUser.username, data);
+    await ColabAPI.sendMail(
+      currentUser.username,
+      projectName,
+      user.email,
+      "collaboration",
+      `${process.env.FRONT_END_BASE_URL || "http://localhost:3000"}/dashboard`
+    );
     setStatus(true);
   };
 
@@ -36,25 +66,41 @@ function UserRequestCard({ user, projectId, owner }) {
             {user.firstName} {user.lastName}
           </p>
           {isOwner && (
-            <button className="btn btn-outline-primary text-uppercase float-right" disabled>
+            <button
+              className="btn btn-outline-primary text-uppercase float-right"
+              disabled
+            >
               Project Owner
             </button>
           )}
-          {!isOwner && (requests.length === 0 || requests[requests.length - 1].accepted === false) && (
-            <button onClick={handleClick} className="btn btn-primary text-uppercase float-right">
-              Request
-            </button>
-          )}
-          {requests[requests.length - 1] && requests[requests.length - 1].accepted === null && (
-            <button className="btn btn-warning text-uppercase float-right" disabled>
-              Request Pending
-            </button>
-          )}
-          {requests[requests.length - 1] && requests[requests.length - 1].accepted === true && (
-            <button className="btn btn-outline-secondary text-uppercase float-right" disabled>
-              Already a collaborator
-            </button>
-          )}
+          {!isOwner &&
+            (requests.length === 0 ||
+              requests[requests.length - 1].accepted === false) && (
+              <button
+                onClick={handleClick}
+                className="btn btn-primary text-uppercase float-right"
+              >
+                Request
+              </button>
+            )}
+          {requests[requests.length - 1] &&
+            requests[requests.length - 1].accepted === null && (
+              <button
+                className="btn btn-warning text-uppercase float-right"
+                disabled
+              >
+                Request Pending
+              </button>
+            )}
+          {requests[requests.length - 1] &&
+            requests[requests.length - 1].accepted === true && (
+              <button
+                className="btn btn-outline-secondary text-uppercase float-right"
+                disabled
+              >
+                Already a collaborator
+              </button>
+            )}
         </div>
       </div>
     </div>
--- src/api/colabApi.js
+++ src/api/colabApi.js
@@ -21,102 +21,140 @@ class ColabAPI {
     }
   }
 
-  //   user API routes
+  //   get all users from database filtered on given username
   static async getAllUsers(username = "") {
     const data = { username };
     let response = await this.request("users", data);
     return response;
   }
-  static async getAllSections() {
-    let response = await this.request("sections");
 
-    return response.sections;
-  }
+  // get single user details from database with given username
   static async getUser(username) {
     let response = await this.request(`users/${username}`);
     return response.user;
   }
+
+  // update a user in the database with passed data
   static async updateUser(username, data) {
     let response = await this.request(`users/${username}`, data, "patch");
     return response.user;
   }
+
+  // login a user with valid username/password combo to return a JWT
   static async loginUser(data) {
     let response = await this.request(`users/login`, data, "POST");
     return response.token;
   }
 
+  // add new user to database with valid data and return JWT
   static async registerUser(data) {
     let response = await this.request(`users/register`, data, "post");
     return response.token;
   }
 
+  // retrieve single project from database
   static async getProject(id) {
     let response = await this.request(`projects/${id}`);
     return response.project;
   }
 
+  // retrieve basic details of single project from database
   static async getProjectDetails(id) {
     let response = await this.request(`projects/${id}/basic`);
     return response.project;
   }
+
+  // get random quote
   static async getQuote() {
     let response = await this.request(`quote`);
     return response.foundQuote;
   }
 
+  // find rhymes for passed word
   static async getRhymes(word) {
     let response = await this.request(`rhymes/${word}`);
     return response.foundRhymes;
   }
+
+  static async sendMail(
+    senderUN,
+    projectName,
+    toEmail,
+    mailEndpoint,
+    dashboard_url
+  ) {
+    const data = { senderUN, projectName, toEmail, dashboard_url };
+    let response = await this.request(`mailer/${mailEndpoint}`, data, "post");
+    return response;
+  }
+
+  // retrieve all sections from database
   static async getSections() {
     let response = await this.request("sections");
     return response.sections;
   }
+
+  // retrieve project arrangement in its current state from database
   static async getArrangement(projectId) {
     let response = await this.request(`arrangements/${projectId}`);
     return response.arrangement;
   }
+
+  // update project arrangement in database with new state
   static async updatedProjectArrangement(projectId, data) {
     let response = await this.request(`arrangements/${projectId}`, data, "put");
     return response;
   }
 
+  // create a new project in the database with the given title and owner
   static async createNewProject(title, owner) {
     let response = await this.request("projects/new", { title, owner }, "post");
     return response.newProject;
   }
+
+  // update project in database with provided data
   static async updatedProject(projectId, data) {
     let response = await this.request(`projects/${projectId}`, data, "patch");
     return response;
   }
 
+  // delete project from database
   static async deleteProject(projectId) {
     let response = await this.request(`projects/${projectId}`, {}, "delete");
     return response;
   }
 
+  // leave from a cowrite
   static async leaveProject(data) {
     let response = await this.request(`cowrites`, data, "delete");
     return response;
   }
 
+  // get all requests for a given user
   static async getUserRequests(username) {
     let response = await this.request(`requests/${username}`);
     return response;
   }
+
+  // get all requests associated with a given project
   static async getProjectRequests(projectId) {
     let response = await this.request(`requests/projects/${projectId}`);
     return response;
   }
+
+  // make a new request between two users for a given project
   static async makeRequest(sender, data) {
     let response = await this.request(`requests/${sender}/new`, data, "post");
     return response;
   }
+
+  // accept a request to collaborate
   static async acceptRequest(requestId) {
     const data = { response: "accept" };
     let response = await this.request(`requests/${requestId}`, data, "put");
     return response;
   }
+  // reject request to collaborate
   static async rejectRequest(requestId) {
     const data = { response: "reject" };
     let response = await this.request(`requests/${requestId}`, data, "put");
